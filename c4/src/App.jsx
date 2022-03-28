167cb663149ab807768c1493138986268ac2057b
MAYUR_KHACHANE <mayurkhachane67@gmail.com>
2022-03-28 16:30:44 +0530

    done

--- c4/src/App.jsx
+++ c4/src/App.jsx
@@ -6,19 +6,17 @@ function App() {
   
   const [users, setusers]= useState([])  
 
-  useEffect(() =>{
-    axios.get("http://localhost:8080/users").then(({data}) =>{
-      setusers(data);
-      console.log(users);
-    })
-  })
+  // useEffect(() =>{
+  //   axios.get("http://localhost:8080/users").then(({data}) =>{
+  //     setusers(data);
+  //     console.log(users);
+  //   })
+  // })
 
   return (
     <div className="App">
       <Navbar/>
-      <div>
-        <button>Home</button>
-      </div>
+      
     </div>
   )
 }
