9c9aba33d992d793b6f1f4ce6fca3e97c69a59cb
MAYUR_KHACHANE <mayurkhachane67@gmail.com>
2022-03-28 16:25:44 +0530

    done

---
+++ c4/src/App.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  
+  const [users, setusers]= useState([])  
+
+  useEffect(() =>{
+    axios.get("http://localhost:8080/users").then(({data}) =>{
+      setusers(data);
+      console.log(users);
+    })
+  })
+
+  return (
+    <div className="App">
+      <Navbar/>
+      <div>
+        <button>Home</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ c4/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='nav'>
+        <button>Home</button>
+        <button>Login</button>
+        <button>Signup</button>
+        <button>CreateMeet</button>
+
+    </div>
+  )
+}
+
+export default Navbar
